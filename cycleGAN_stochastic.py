Fixd NLL bug & Added Spectral Normalization

cycleGAN_stochastic.py
@@ -27,7 +27,7 @@ def sample_z(mu, logvar):
     return mu + torch.exp(logvar / 2) * eps
 
 def NormalNLLLoss(x, mu, logvar):
-    NormalNLL =  -1 * (-0.5 * (2*np.log(np.pi) + logvar) - (x - mu)**2 / (2*torch.exp(logvar/2)))
+    NormalNLL =  -1 * (-0.5 * (np.log(2*np.pi) + logvar) - (x - mu)**2 / (2*torch.exp(logvar)))
     return NormalNLL
 
 class Encoder(nn.Module):

mapGAN.py
@@ -11,6 +11,21 @@ from padding_same_conv import Conv2d
 import numpy as np
 import matplotlib.pyplot as plt
 
+def Interpo_dist_loss(x1, x2, xm):
+    d12 = ((x1 - x2)**2).sum([1])
+    d1m = ((x1 - xm)**2).sum([1])
+    d2m = ((x2 - xm)**2).sum([1])
+    dist_loss1 = (d1m - d12).mean().clamp(min=0)
+    dist_loss2 = (d2m - d12).mean().clamp(min=0)
+    dist_loss = dist_loss1 + dist_loss2
+    return dist_loss
+
+def Rate_dist_loss(z1, z2, x1, x2):
+    dz12 = ((z1 - z2)**2).mean([1])
+    dx12 = ((x1 - x2)**2).mean([1,2,3])
+    dist_loss = nn.MSELoss()(dz12, dx12)
+    return dist_loss.mean()
+
 # Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
@@ -22,95 +37,49 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-def adaIN_Layer(h_conv, w, aff_s, aff_b):
-    channel = h_conv.size(1)   
-    h_s = torch.tanh(aff_s(w).view(-1,channel,1,1))
-    h_b = torch.tanh(aff_b(w).view(-1,channel,1,1))
-    #noise = torch.randn(h_conv.size()).to(device)
-    #h_conv = h_conv + noise
-    h_norm = torch.nn.InstanceNorm2d(channel)(h_conv)
-    h_style = h_s * h_norm + h_b
-    
-    return h_style
-
 class Generator(nn.Module):
-    def __init__(self, z_dim):
+    def __init__(self, z_dim, ndf=128):
         super(Generator, self).__init__()
-        # Trainable Constant Input
-        self.const = nn.Parameter(torch.randn(1, 512, 4, 4, device='cuda:0'))
-        
+        self.ndf = ndf
         # Mapping Network
-        self.fc1 = nn.Linear(z_dim, z_dim)
-        self.fc2 = nn.Linear(z_dim, z_dim)
-        
-        # adaIN Affine Transform
-        # 8x8
-        self.aff1_s = nn.Linear(z_dim, 256, bias=False)
-        self.aff1_b = nn.Linear(z_dim, 256, bias=False)
-        # 16x16
-        self.aff2_s = nn.Linear(z_dim, 128, bias=False)
-        self.aff2_b = nn.Linear(z_dim, 128, bias=False)
-        # 32x32
-        self.aff3_s = nn.Linear(z_dim, 64, bias=False)
-        self.aff3_b = nn.Linear(z_dim, 64, bias=False)
-        # 64x64
-        self.aff4_s = nn.Linear(z_dim, 32, bias=False)
-        self.aff4_b = nn.Linear(z_dim, 32, bias=False)
-
-        # Upsample Convolution Layer
-        # 4x4x512 -> 8x8x256
-        self.up1 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.conv1 = Conv2d(512, 256, 3)
-        self.bn1 = nn.BatchNorm2d(256)
-        # 8x8x256 -> 16x16x128
+        self.fc_map1 = nn.Linear(z_dim, z_dim)
+        self.fc_map2 = nn.Linear(z_dim, z_dim)
+        self.fc_map3 = nn.Linear(z_dim, z_dim)
+        # Generation Network
+        self.fc1 = nn.Linear(z_dim, 8*8*ndf*8)
+        self.conv1 = Conv2d(ndf*8, ndf*4, 3)
+        # (512, 8, 8)
         self.up2 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.conv2 = Conv2d(256, 128, 3)
-        self.bn2 = nn.BatchNorm2d(128)
-        # 16x16x128 -> 32x32x64
+        self.conv2 = Conv2d(ndf*4, ndf*2, 3)
+        self.bn2 = nn.BatchNorm2d(ndf*2)
+        # (256, 16, 16)
         self.up3 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.conv3 = Conv2d(128, 64, 5)
-        self.bn3 = nn.BatchNorm2d(64)
-        # 32x32x64 -> 64x64x32
+        self.conv3 = Conv2d(ndf*2, ndf, 5)
+        self.bn3 = nn.BatchNorm2d(ndf)
+        # (128, 32, 32)
         self.up4 = nn.Upsample(scale_factor=2, mode='nearest')
-        self.conv4 = Conv2d(64, 32, 5)
-        self.bn4 = nn.BatchNorm2d(128)
-        
-        # Color Transform Layer
-        self.conv1_rgb = Conv2d(256, 3, 3)
-        self.conv2_rgb = Conv2d(128, 3, 3)
-        self.conv3_rgb = Conv2d(64, 3, 3)
-        self.conv4_rgb = Conv2d(32, 3, 1)
+        self.conv4 = Conv2d(ndf, 3, 5)
+        # (3, 64, 64)
 
     def forward(self, z):
-        self.h_map1 = F.relu(self.fc1(z))
-        self.w = F.relu(self.fc2(self.h_map1))
-        # 4x4
-        self.const_feat = self.const.view(-1,512,4,4).repeat(z.size(0),1,1,1)
-        self.h_conv1 = self.up1(self.const_feat)
-        self.h_conv1 = self.conv1(self.h_conv1)
-        self.h_conv1 = adaIN_Layer(self.h_conv1, self.w, self.aff1_s, self.aff1_b)
-        self.h_rgb1 = torch.sigmoid(self.conv1_rgb(self.h_conv1))
-        self.h_conv1 = F.relu(self.h_conv1)
-        # 8x8
-        self.h_conv2 = self.up2(self.h_conv1)
-        self.h_conv2 = self.conv2(self.h_conv2)
-        self.h_conv2 = adaIN_Layer(self.h_conv2, self.w, self.aff2_s, self.aff2_b)
-        self.h_rgb2 = torch.sigmoid(self.conv2_rgb(self.h_conv2))
-        self.h_conv2 = F.relu(self.h_conv2)
-        # 16x16
-        self.h_conv3 = self.up3(self.h_conv2)
-        self.h_conv3 = self.conv3(self.h_conv3)
-        self.h_conv3 = adaIN_Layer(self.h_conv3, self.w, self.aff3_s, self.aff3_b)
-        self.h_rgb3 = torch.sigmoid(self.conv3_rgb(self.h_conv3))
-        self.h_conv3 = F.relu(self.h_conv3)
-        # 32x32
-        self.h_conv4 = self.up4(self.h_conv3)
-        self.h_conv4 = self.conv4(self.h_conv4)
-        self.h_conv4 = adaIN_Layer(self.h_conv4, self.w, self.aff4_s, self.aff4_b)
-        self.h_rgb4 = torch.sigmoid(self.conv4_rgb(self.h_conv4))
-        # 64x64
-        #return h_rgb1, h_rgb2, h_rgb3, h_rgb4 
-        return self.h_rgb4
+        h_map1 = F.relu(self.fc_map1(z))
+        h_map2 = F.relu(self.fc_map2(h_map1))
+        w = self.fc_map3(h_map2)
+        #
+        h_fc1 = F.relu(self.fc1(w).view(-1,self.ndf*8,8,8)) 
+        h_conv1 = F.relu(self.conv1(h_fc1))
+        # (512, 8, 8)
+        h_conv2 = self.up2(h_conv1)
+        h_conv2 = F.relu(self.bn2(self.conv2(h_conv2)))
+        # (256, 16, 16)
+        h_conv3 = self.up3(h_conv2)
+        h_conv3 = F.relu(self.bn3(self.conv3(h_conv3)))
+        # (128, 32, 32)
+        h_conv4 = self.up4(h_conv3)
+        h_conv4 = self.conv4(h_conv4)
+        x_samp = torch.sigmoid(h_conv4)
+        # (3, 64, 64)
+        return x_samp, w
 
 class Discriminator(nn.Module):
     def __init__(self, ndf=128):
@@ -131,7 +100,7 @@ class Discriminator(nn.Module):
         d_prob = torch.sigmoid(d_logit)
         return d_prob, d_logit
 
-z_dim = 64
+z_dim = 128
 num_epochs = 20
 
 netG = Generator(z_dim).to(device)
@@ -141,11 +110,11 @@ netD.apply(weights_init)
 
 criterion = nn.BCELoss()
 optD = optim.Adam(netD.parameters(), lr=1e-4, betas=(0.5, 0.999))
-optG = optim.Adam(netG.parameters(), lr=2e-4, betas=(0.5, 0.999))
+optG = optim.Adam(netG.parameters(), lr=4e-4, betas=(0.5, 0.999))
 
 z_fixed = torch.randn(64, z_dim, device=device)
 
-model_name = "styleGAN"
+model_name = "mapgan"
 out_folder = "out/" + model_name + "/"
 if not os.path.exists(out_folder):
     os.makedirs(out_folder)
@@ -169,7 +138,7 @@ for epoch in range(num_epochs):
 
         # Fake Batch
         z_samp = torch.randn(b_size, z_dim, device=device)
-        x_samp = netG(z_samp)
+        x_samp, w = netG(z_samp)
         d_fake, _ = netD(x_samp)
         d_label.fill_(0.0)
         d_fake_loss = criterion(d_fake, d_label)
@@ -183,20 +152,34 @@ for epoch in range(num_epochs):
         netG.zero_grad()
         d_label.fill_(1.0)
         z_samp = torch.randn(b_size, z_dim, device=device)
-        x_samp = netG(z_samp)
+        x_samp, _ = netG(z_samp)
         d_fake, _ = netD(x_samp)
         g_loss = criterion(d_fake, d_label)
         g_loss.backward()
+        #optG.step()
+
+        # Distance Loss
+        z1_samp = torch.randn(b_size, z_dim, device=device)
+        x1_samp, w1 = netG(z1_samp)
+        z2_samp = torch.randn(b_size, z_dim, device=device)
+        x2_samp, w2 = netG(z2_samp)
+        zm_samp = 0.5*z1_samp + 0.5*z2_samp
+        xm_samp, wm = netG(zm_samp)
+
+        interpo_loss = Interpo_dist_loss(w1, w2, wm)
+        rate_loss = Rate_dist_loss(w1, w2, x1_samp, x2_samp)
+        dist_loss = 1e-3*interpo_loss + rate_loss
+        dist_loss.backward()
         optG.step()
 
         # Results
         if i % 50 == 0:
-            print("[%d/%d][%s/%d] D_loss: %.4f | G_loss: %.4f"\
-            %(epoch+1, num_epochs, str(i).zfill(4), len(dataloader), d_loss.item(), g_loss.item()))
+            print("[%d/%d][%s/%d] D_loss: %.4f | G_loss: %.4f | Interpo_loss: %.4f | Rate_loss: %.4f"\
+            %(epoch+1, num_epochs, str(i).zfill(4), len(dataloader), d_loss.item(), g_loss.item(), interpo_loss.item(), rate_loss.item()))
         
         if i%200 == 0:
             # Output Images
-            x_fixed = netG(z_fixed).cpu().detach()
+            x_fixed = netG(z_fixed)[0].cpu().detach()
             plt.figure(figsize=(8,8))
             plt.imshow(np.transpose(vutils.make_grid(x_fixed, padding=2, normalize=True).cpu(),(1,2,0)))
             plt.axis("off")